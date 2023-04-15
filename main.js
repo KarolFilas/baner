001f1f065808858e341df6ff2e3aa13f53be1978
Karol Filas <filaskarol@gmail.com>
2023-04-15 14:13:24 +0200

    changing dots with a slides

--- main.js
+++ main.js
@@ -11,9 +11,16 @@ const slideList = [{
 
 const image = document.querySelector('img.slider')
 const h1 = document.querySelector('h1.slider')
+const dots = [...document.querySelectorAll('.dots span')]
 // interface
 const time = 3000
 let active = 0
+// implementacja
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'))
+    dots[activeDot].classList.remove('active')
+    dots[active].classList.add('active')
+}
 
 const changeSlide = () => {
     active++
@@ -22,16 +29,7 @@ const changeSlide = () => {
     }
     image.src = slideList[active].img
     h1.textContent = slideList[active].text
-
+    changeDot()
 }
 
-
-
-
-
 setInterval(changeSlide, time)
-
-
-
-
-// implementacja
